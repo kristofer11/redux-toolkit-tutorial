ca39a34d2544d042b10b3dda574bb8347c473a3f
Kris Hvattum <klhvattum@live.com>
2023-10-04 14:34:14 -0700

    add navbar

--- vite-starter/src/main.jsx
+++ vite-starter/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { store } from './store';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
@@ -8,7 +9,10 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+
+    </React.StrictMode>
 );
